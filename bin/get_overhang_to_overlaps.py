b9c3e225613574d7afdc2996d9d8f1a0d0d7e050
alibashir <bashia02@login1.mothra.hpc.mssm.edu>
2016-01-29 18:55:02 -0500

    added in alternative way of rerunning and analyzing blasr for cases in which original bam was not reun with high bestn

--- bin/get_overhang_to_overlaps.py
+++ bin/get_overhang_to_overlaps.py
@@ -4,6 +4,7 @@ from Overlap import overlap
 from collections import Counter
 
 BLASR = "/sc/orga/projects/bashia02b/tools/bin/blasr"
+NPROC = 1
 
 def constructOverlap (bamfile, ah, edge, tolerance):
     qid = ah.query_name
@@ -27,7 +28,7 @@ def checkReadOverhang(bamfile, ah, edge, tolerance):
         return True
     return False
 
-def findContigOverhangReads (bamfile, fastafile, contig, edge=2000, tolerance=200):
+def findContigOverhangReads (bamfile, fastafile, contig, seqfn=False, fastafn=None, edge=2000, tolerance=200):
     
     end = fastafile.get_reference_length(contig)
     tempreadsleft = bamfile.fetch(contig, 0, edge)
@@ -44,7 +45,7 @@ def findContigOverhangReads (bamfile, fastafile, contig, edge=2000, tolerance=20
             except:
                 pass
             leftreadsDict[ah.qname] = hap
-
+            reads.append((ah.qname, ah.seq))
     for ah in tempreadsright:
         if checkReadOverhang(bamfile, ah, edge, tolerance):
             hap = 0
@@ -53,10 +54,30 @@ def findContigOverhangReads (bamfile, fastafile, contig, edge=2000, tolerance=20
             except:
                 pass
             rightreadsDict[ah.qname] = hap
+            reads.append((ah.qname, ah.seq))
+
+    if seqfn:
+        # write out sequence to fasta
+        seq_fa_fn = seqfn+".fa"
+        rout = open(seq_fa_fn, 'w')
+        print >>rout, "\n".join(map(lambda x: ">%s\n%s" %(x[0], x[1]), reads))
+        rout.close()
+        
+        # run blasr
+        blasrcmd = "%s -nproc %s -sam -out %s.sam -bestn 20 -noSplitSubreads -clipping soft %s %s" %(BLASR, NPROC, seqfn, seq_fa_fn, fastafn)
+        # convert to bam
+        samcmd = "samtools view -Sbh %s.sam > %s.bam" %(seqfn, seqfn)
+        # sort
+        sortcmd = "samtools sort %s.bam %s.sorted" %(seqfn, seqfn)
+        # index
+        indexcmd = "samtools index %s.sorted.bam" %(seqfn)
+        for cmd in [blasrcmd, samcmd, sortcmd, indexcmd]:
+            print >>sys.stderr, cmd
+            os.system(cmd)
 
     return leftreadsDict, rightreadsDict
 
-def AltAnchoredContigs (bamfile, bam_indexer,  fasta_file, alt_contigs, contig, read_dict):
+def AltAnchoredContigs (bamfile, bam_indexer,  fasta_file, alt_contigs, contig, read_dict, edge=2000, tolerance=200):
     '''
     Goal is to take in a list of alt contigs and potential
     reads and find out if the read is uniquely anchored to this 
@@ -69,7 +90,7 @@ def AltAnchoredContigs (bamfile, bam_indexer,  fasta_file, alt_contigs, contig, 
         for ah in read_indexer:
             tid = bamfile.getrname(ah.reference_id)
             if tid != contig: # ignore if its the seed contig
-                ov = constructOveralp(ah)
+                ov = constructOverlap(bamfile, ah, edge, tolerance)
                 if ov.hasFullOverlap(): overlap_contigs.add(tid)
         if len(overlap_contigs) == 1:
             haplotypes.setdefault(hap, Counter())[tid] += 1
@@ -80,12 +101,12 @@ def printContigSummary(contig, leftjoins, rightjoins):
     print contig
     print "LEFT HAPLOTYPES:"
     for haplotype, contigCounts in leftjoins.items():
-        print "-",haplo
+        print "-",haplotype
         for contig, contigCount in contigCounts.items():
             print "--", contig, contigCount
     print "RIGHT HAPLOTYPES:"
     for haplotype, contigCounts in rightjoins.items():
-        print "-",haplo
+        print "-",haplotype
         for contig, contigCount in contigCounts.items():
             print "--", contig, contigCount
     print "#"*80
@@ -100,7 +121,6 @@ def read_branch_tigs (fn):
     with open(fn) as bf:
         bf.readline()
         for l in bf:
-            print l
             ll = l.strip().split(" ")
             bf_dict[ll[0]]=ll[2:]
     return bf_dict
@@ -115,18 +135,28 @@ def run ():
     fastafile = pysam.FastaFile(fasta_fn)
     contig_dict = read_branch_tigs (contig_pair_file)
 
-    # index by read name
-    print >>sys.stderr, "Indexing bamfile"
-    indexer = pysam.IndexedReads(bamfile)
-    indexer.build()
-
     print >>sys.stderr, "Iterating over contigs"
     for contig, alt_contigs in contig_dict.items():
-        leftreads, rightreads = findContigOverhangReads(bamfile, fastafile, contig) 
+        print >>sys.stderr, contig, alt_contigs
+        
+        # create a temp sequence file 
+        temp_seq_fn = "temp_blasr"
+        temp_bam_fn = "temp_blasr.sorted.bam"
+        
+        # get overhang reads
+        leftreads, rightreads = findContigOverhangReads(bamfile, fastafile, contig, seqfn=temp_seq_fn, fastafn=fasta_fn) 
         print >>sys.stderr, leftreads
         print >>sys.stderr, rightreads
-        leftjoins = AltAnchoredContigs (bamfile, indexer, fastafile, alt_contigs, contig, leftreads)
-        rightjoins = AltAnchoredContigs (bamfile, indexer, fastafile, alt_contigs, contig, rightreads)
+
+        # index by read name
+        print >>sys.stderr, "Indexing bamfile"
+        smallbamfile = pysam.AlignmentFile(temp_bam_fn)
+        indexer = pysam.IndexedReads(smallbamfile)
+        indexer.build()
+
+        # get alt anchors
+        leftjoins = AltAnchoredContigs (smallbamfile, indexer, fastafile, alt_contigs, contig, leftreads)
+        rightjoins = AltAnchoredContigs (smallbamfile, indexer, fastafile, alt_contigs, contig, rightreads)
         printContigSummary(contig, leftjoins, rightjoins)
 
 run()        
